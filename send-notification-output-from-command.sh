6c58d795a4d71bd4845c2fe3620fbe35c7b4aad9
Thiện Lâm <lamminhthien02012000@gmail.com>
2024-04-29 21:34:16 +0700

    change url to secret

--- send-notification-output-from-command.sh
+++ send-notification-output-from-command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Define google chat webhook link
-GOOGLE_CHAT_CHANNEL_DEMO="https://chat.googleapis.com/v1/spaces/AAAAs6OfqwY/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=X_NVTaKzl589GsA4GQ5bdwYzn-8qDw1bDYeqXvkGqnQ"
+GOOGLE_CHAT_CHANNEL_DEMO="SECRET"
 
 # Send notification
 curl \
