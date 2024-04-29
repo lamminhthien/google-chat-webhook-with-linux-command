4d68aeef127d4e54654173163d220f73cc4bab13
Thiện Lâm <lamminhthien02012000@gmail.com>
2024-04-29 22:10:14 +0700

    example notification about renew ssl

--- send-notification-output-from-command.sh
+++ send-notification-output-from-command.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-# Define google chat webhook link, please run command export first to set variable environment first
-# export GOOGLE_CHAT_CHANNEL_DEMO=https://chat.googleapis.com/v1/spaces/...
 
-# Mac OS, create a variable to store output of command "brew list"
-BREW_LIST=$(brew list)
-
-# Send notification
+# Send notification about renew SSL certificate
 curl \
 -X POST \
 -H 'Content-Type: application/json' \
-"$GOOGLE_CHAT_CHANNEL_DEMO" \
--d '{"text": "'"$BREW_LIST"'"}'
+"https://chat.googleapis.com/v1/spaces/SPACE_ID/messages?key=message_key&token=message_token" \
+-d '{"text": "'"$(sudo certbot renew --nginx)"'"}'
