09660130c9c731fa4c9627dc21224e4830c19fe6
Thiện Lâm <lamminhthien02012000@gmail.com>
2024-04-29 21:47:20 +0700

    add example of send output from linux or macos command

--- send-notification-output-from-command.sh
+++ send-notification-output-from-command.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# Define google chat webhook link
-GOOGLE_CHAT_CHANNEL_DEMO="SECRET"
+# Define google chat webhook link, please run command export first to set variable environment first
+# export GOOGLE_CHAT_CHANNEL_DEMO=https://chat.googleapis.com/v1/spaces/...
+
+# Mac OS, create a variable to store output of command "brew list"
+BREW_LIST=$(brew list)
 
 # Send notification
 curl \
 -X POST \
 -H 'Content-Type: application/json' \
-"${GOOGLE_CHAT_CHANNEL_DEMO}" \
--d '{"text": "Hello from Witcentre!"}'
+"$GOOGLE_CHAT_CHANNEL_DEMO" \
+-d '{"text": "'"$BREW_LIST"'"}'
